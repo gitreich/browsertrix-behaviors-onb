a4689d6cd2e1a21f5a2fa02bb3cc44e82eb79b89
Antares Reich <antares.reich@onb.ac.at>
2023-12-01 12:57:29 +0100

    Regex zm erkennen der ORF Seite um Landes Subdomains und topos erweitert

--- src/site/news_orf_at.ts
+++ src/site/news_orf_at.ts
@@ -1,32 +1,25 @@
-import {getState, sleep, xpathString} from "../lib/utils";
+import {TikTokVideoBehavior} from "./tiktok";
 
 const Q = {
   orfMain: "//main[@id=\"content\"]",
-  orfVideosDivClass: "div[contains(@class, 'videolane-item')]",
+  orfVideosDivClass: "//div[contains(@class, 'videolane-item')]",
   orfVideosPlayButtonClass: "*[contains(@class, 'videolane-huge-play-button')]",
-  orfEmbedVideoClass: "div[contains(@class, 'oonmedia-video-container')]",
+  orfEmbedVideoClass: "//div[contains(@class, 'oonmedia-video-container')]",
   orfEmbedVideoPlayClass: "//button[contains(@class, 'bmpui-ui-playbacktogglebutton')]",
   orfEmbedVideoLength: "//span[contains(@class, 'bmpui-text-right')]",
-
-  adsReportDownloadTimeSpan: "//*[@id=\"ReportDownload\"]/div/div/div/div/div[1]/div[2]/div/div[2]/div[1]/div[2]/div/span/div/div/div[2]/div",
-  adsReportTimeBody: "/html/body/div[5]/div[1]/div[1]/div/div/div[1]/div[2]/div",
-  adsReportDownloadLastDay: "div[1]",
-  adsReportDownloadLast7Days: "div[2]",
-  adsReportDownloadLast30Days: "div[3]",
-  adsReportDownloadAllDays: "div[5]",
-  adsReportDownloadApply: "//*[@id=\"facebook\"]/body/div[5]/div[2]/div[3]/div/div[2]/div/span/div/div/div",
-  adsReportDownload: "//*[@id=\"ReportDownload\"]/div/div/div/div/div[1]/div[2]/div/div[2]/div[4]/a",
+  orfEmbededLiveStream: "//div[contains(@class, 'bmpui-oon-livestream')]",
+  orfNewsroomTimeline: "//li[contains(@class, 'list-item as-timeline')]",
 };
 
 export const BREADTH_ALL = Symbol("BREADTH_ALL");
 export class NewsOrfClickVideosBehavior {
-
   static id = "NewsOrfClickVideos";
 
   isMobile: boolean;
 
   static isMatch() {
-    return !!window.location.href.match(/https:\/\/(www\.)?orf\.at/);
+    return !!window.location.href.match(/https:\/\/(www\.)?([a-z]+\.)?orf\.at/);
+    // TODO: Skip Livestream: livestream
   }
 
   static init() {
@@ -47,22 +40,14 @@ export class NewsOrfClickVideosBehavior {
 
     const orfMain = xpathNode(Q.orfMain);
 
-    console.log(orfMain)
+    console.log(orfMain);
 
     // Videos of Newsroom
-    const videoItems = iterChildMatches(Q.orfVideosDivClass, orfMain);
-    for await (const video of videoItems) {
-      console.log(video);
-      const video_button = xpathNode(Q.orfVideosPlayButtonClass, video);
-      console.log(video_button);
-      await scrollAndClick(video_button);
-      yield getState(ctx, "View Video", "Videos");
 
-    }
 
     // Videos in Articles
     const embedVideoItems = iterChildMatches(Q.orfEmbedVideoClass, orfMain);
-    console.log(embedVideoItems)
+    console.log(embedVideoItems);
     for await (const video of embedVideoItems) {
       console.log(video);
       const video_button = xpathNode(Q.orfEmbedVideoPlayClass, video);
@@ -84,20 +69,94 @@ export class NewsOrfClickVideosBehavior {
 
   }
 
+  async* clickOrfNewsroomVideo(ctx, video) {
+    const {getState, scrollAndClick, xpathNode} = ctx.Lib;
+
+    console.log(video);
+    const video_button = xpathNode(Q.orfVideosPlayButtonClass, video);
+    console.log(video_button);
+    if (!video_button) return;
+    await scrollAndClick(video_button);
+    yield getState(ctx, "View Video", "Videos");
+  }
+
+
+  async* clickEmbededOrfVideo(ctx, video) {
+    const {getState, xpathString, scrollAndClick, xpathNode, waitRandom} = ctx.Lib;
+
+    yield getState(ctx, "Starting Clicking ORF Video");
+    // const orfMain = xpathNode(Q.orfMain);
+    // const cvideo = xpathNode(video, orfMain);
+
+
+    console.log(video);
+    const video_button = xpathNode(Q.orfEmbedVideoPlayClass, video);
+    if (!video_button) return;
+    await scrollAndClick(video_button);
+    await waitRandom();
+    const video_length = xpathString(Q.orfEmbedVideoLength, video);
+    const time = video_length.split(":");
+    var wait = +time[0] * 60 + +time[1];
+    wait = wait * 1000;
+    yield getState(ctx, "Video started waiting for " + wait + " ms", "Videos");
+    await new Promise(f => setTimeout(f, wait ));
+
+    yield getState(ctx, "Video awaited, continue");
+  }
+
+  async* openVideo(ctx, item) {
+    const { HistoryState, xpathNode, sleep } = ctx.Lib;
+    const link = xpathNode(".//a", item);
+    if (!link) return;
+    const viewState = new HistoryState(() => link.click());
+    await sleep(500);
+    if (viewState.changed) {
+      const videoBehavior = new TikTokVideoBehavior();
+      yield* videoBehavior.run(ctx);
+      await sleep(500);
+      //await viewState.goBack(Q.backButton);
+    }
+  }
+
   async* run(ctx) {
-    const {getState, waitRandom, isMobile} = ctx.Lib;
+    const {getState,  isMobile, xpathNode, xpathNodes } = ctx.Lib;
 
     this.isMobile = isMobile();
 
     yield getState(ctx, "Starting bahviors ONB with isMobile " + this.isMobile );
 
-    await waitRandom();
-
     ctx.state = {"Videos": 0};
 
-    yield* this.clickAllOrfVideos(ctx);
-    await waitRandom();
+    var vids = new Array();
 
+    const orfMain = xpathNode(Q.orfMain);
+
+    const videoItems = xpathNodes(Q.orfVideosDivClass, orfMain);
+    for await (const video of videoItems) {
+      yield* await this.clickOrfNewsroomVideo(ctx,video);
+    }
+
+
+
+    const embedVideoItems = xpathNodes(Q.orfEmbedVideoClass, orfMain);
+    for await (const video of embedVideoItems) {
+      const livestream = xpathNode(Q.orfEmbededLiveStream, video);
+      if (livestream) {
+        console.log(livestream);
+        continue;
+      }
+      else {
+        this.openVideo(ctx, video);
+        vids.push(video);
+      }
+
+
+    }
+
+    console.log(embedVideoItems);
+    for (const video of vids) {
+      yield* await this.clickEmbededOrfVideo(ctx,video);
+    }
 
     yield "ORF Videos Click Complete";
   }
